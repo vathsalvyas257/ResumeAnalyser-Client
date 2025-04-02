02934da19d8330dbcd5cc19d84d7cc947da1b180
StarLord <rameswarreddy78@gmail.com>
2025-04-02 15:20:06 +0530

    feat:responsive navbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <div className="flex-grow">
+      <div>
+        <Navbar />
+      </div>
+      <div className="flex-grow mt-[120px]">
         <Outlet />
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Body;
